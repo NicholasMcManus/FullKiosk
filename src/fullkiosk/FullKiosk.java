3e5088052684b6980bfb7d8468a78aa54ed20233
NicholasMcManus <mcmanusnj3@gmail.com>
2020-09-11 15:43:11 -0400

    Initial Commit

---
+++ src/fullkiosk/FullKiosk.java
@@ -0,0 +1,54 @@
+/*
+ * file name: FullKiosk.java
+ * programmer name: Nick McManus
+ * date created: 09-11-2020
+ * date of last revision: 
+ * details of last revision:
+ * short description: 
+ */
+package fullkiosk;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Nick McManus
+ */
+public class FullKiosk extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
